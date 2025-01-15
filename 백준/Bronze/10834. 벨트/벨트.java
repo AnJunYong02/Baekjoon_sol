63a50729fdee27f21296e8ef8659fe33202cc682
JunYong An <103262585+AnJunYong02@users.noreply.github.com>
2025-01-15 22:02:08 +0900

    [Bronze II] Title: 벨트, Time: 144 ms, Memory: 16368 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10834. 벨트/벨트.java
+++ 백준/Bronze/10834. 벨트/벨트.java
@@ -1,23 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int M = Integer.parseInt(br.readLine()); //벨트 갯수
         int rotation = 0; //회전방향 (0:시계, 1:반시계)
-        double ratio = 1;
+        int ratio = 1; //벨트 마지막 회전수
 
         for(int i=0; i<M; i++){
-            String[] str = br.readLine().split(" ");
-            int a = Integer.parseInt(str[0]); //i 회전수
-            int b = Integer.parseInt(str[1]); //i+1 회전수
-            int s = Integer.parseInt(str[2]); //꼬였는지 (안꼬이면 0, 꼬이면 1)
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()); //i 회전수
+            int b = Integer.parseInt(st.nextToken()); //i+1 회전수
+            int s = Integer.parseInt(st.nextToken()); //꼬였는지 (안꼬이면 0, 꼬이면 1)
 
             //계산
-            double divide = (double) b /a;
-            ratio = ratio * divide;
+            ratio = (ratio/a) * b;
             if(s==1){ //꼬였다면 회전방향 바뀜
                 if(rotation==1) rotation=0;
                 else rotation = 1;
