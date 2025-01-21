c87fd19129d15d1cbb34efd448a5045bf85ef325
JunYong An <103262585+AnJunYong02@users.noreply.github.com>
2025-01-21 11:48:41 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,30 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> list = new ArrayList<>();
-        int[] dp = new int[N];
+        int[] list = new int[N];
 
         //list에 넣기
-        for(int i=0; i<N; i++) list.add(Integer.parseInt(st.nextToken()));
+        for(int i=0; i<N; i++) list[i] = Integer.parseInt(st.nextToken());
 
         //정렬하기 (오름차순으로) 31432 -> 12334 => 1, 3, 6, 9, 13 = 32
-        Collections.sort(list);
+        Arrays.sort(list);
         //합 출력
-        int sum = 0;
-        dp[0] = list.get(0);
-        sum += dp[0];
+        int[] dp = new int[N];
+        int sum = list[0];
+        dp[0] = list[0];
+
         for(int i=1; i<N; i++) {
-            dp[i] = dp[i-1] + list.get(i);
+            dp[i] = dp[i-1] + list[i];
             sum += dp[i];
         }
         System.out.println(sum);
