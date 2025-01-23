654a61820e3202c5e07b72442d155da64c046026
JunYong An <103262585+AnJunYong02@users.noreply.github.com>
2025-01-23 16:50:22 +0900

    [Silver III] Title: 주유소, Time: 364 ms, Memory: 35388 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,43 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int city = Integer.parseInt(br.readLine()); //도시 수
-        int[] distance = new int[city - 1]; //도시 사이 거리
-        int[] fuelPrice = new int[city]; //도시마다 주유 가격
+        int city = Integer.parseInt(br.readLine()); // 도시 수
+        int[] distance = new int[city - 1]; // 도시 사이 거리
+        int[] fuelPrice = new int[city]; // 도시마다 주유 가격
 
+        // 거리 입력
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < city - 1; i++) {
             distance[i] = Integer.parseInt(st.nextToken());
         }
+
+        // 주유소 가격 입력
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < city; i++) {
             fuelPrice[i] = Integer.parseInt(st.nextToken());
         }
 
-        int totalPrice = 0;
-        int travleCity = 0; //이동가능한 기름 양
-        int minFuel = fuelPrice[0];
-        int index = 0;
+        long totalPrice = 0; // 총 비용 (long으로 변경, 입력 제한이 크기 때문에)
+        int minFuel = fuelPrice[0]; // 첫 번째 도시에서의 최소 기름값
 
-        while (travleCity < city-1) { //도시 끝까지 가기까지
-            if (minFuel > fuelPrice[index+1]) { //싼 도시의 기름값을 만난다면 -> 최소 기름값 바꿈
-                totalPrice += (distance[index] * minFuel); //기름넣는양 = 지금도시 거리 * 기름 값
-                minFuel = fuelPrice[index + 1];
+        for (int i = 0; i < city - 1; i++) {
+            // 현재 도시와 다음 도시까지 거리만큼 기름값 계산
+            totalPrice += (long) distance[i] * minFuel;
+
+            // 최소 기름값 갱신 (다음 도시의 기름값이 더 작다면 갱신)
+            if (fuelPrice[i + 1] < minFuel) {
+                minFuel = fuelPrice[i + 1];
             }
-            else { //더 작은 기름값 만나기까지 계속해서 최소 기름값 더해나감
-                totalPrice += (distance[index] * minFuel); //다음도시까지 거리 * 최소 기름 값
-            }
-            travleCity++;
-            index++;
         }
+
         System.out.println(totalPrice);
     }
 }
